930e77f9b98f5a6370d61bb77c93fc1a8b1d2494
MrPuppeteer <nuriksabagas@gmail.com>
2022-08-08 10:45:21 +0700

    feat: add placeholder to datetime input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,6 +22,7 @@ const Form = ({ onSaveData }) => {
 			<input
 				className="form-input px-5 py-2.5 mr-2 mb-4 rounded-md border border-gray-200 shadow-md"
 				type={'datetime-local'}
+				placeholder="Enter Date & Time"
 				value={datetime}
 				onChange={datetimeChangeHandler}
 			/>
@@ -29,7 +30,7 @@ const Form = ({ onSaveData }) => {
 				className="text-white bg-sky-600 hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-sky-300 font-medium text-sm rounded-md px-5 py-2.5 text-center mb-4"
 				type="submit"
 			>
-				Set Datetime
+				Set Date & Time
 			</button>
 		</form>
 	);
